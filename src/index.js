b560877a9b2dd543f2ee872f0b6d0264afee0419
willeswa <gwiliez@gmail.com>
2019-07-03 11:52:04 +0300

    chore(setup): setup sass processing
    - configure sass processing
    
    [Finishes #166980204]

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
 import store from './store';
 import App from './components/App';
+import '../src/Main';
 
 ReactDOM.render(
   <Provider store={store}>
