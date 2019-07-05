f68417f1a9c9a6bbffdf1ca9ac542132e1748aba
wangonya <kwangonya@gmail.com>
2019-07-05 12:21:09 +0300

    add lock file

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,6 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'public/'),
     port: 3000,
-    publicPath: 'http://localhost:3000/dist/',
     historyApiFallback: true,
   },
   plugins: [HtmlWebpackPluginConfig],
