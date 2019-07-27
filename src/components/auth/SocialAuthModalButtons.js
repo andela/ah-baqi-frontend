40e2a4fdc2b27721953f0748f398c19df8427a1d
wangonya <kwangonya@gmail.com>
2019-07-27 06:41:57 +0300

    shorten SocialAuthModalButtons function

--- src/components/auth/SocialAuthModalButtons.js
+++ src/components/auth/SocialAuthModalButtons.js
@@ -3,19 +3,66 @@ import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props
 import { GoogleLogin } from 'react-google-login';
 import AuthModalButton from './AuthModalButtons';
 
+const googgleClientId = '1005118583442-8vl6g21ovisrkasced1n5ea0vpcms2ge.apps.googleusercontent.com';
+const facebookAppId = '424643588295726';
 const customHeader = {};
 customHeader.Test = 'test-header';
 
+const facebookLogin = (facebookResponse, action) => (
+  <FacebookLogin
+    facebook-button="facebook button"
+    appId={facebookAppId}
+    autoLoad={false}
+    fields="name,email,picture"
+    callback={response => facebookResponse(response, 'facebook')}
+    render={renderProps => (
+      <AuthModalButton
+        iconType="facebook"
+        color="#3C5A99"
+        clicked={renderProps.onClick}
+        theme="filled"
+      >
+        <span className="spaceRight">
+          {action}
+          {' '}
+        </span>
+        {' '}
+            with Facebook
+      </AuthModalButton>
+    )}
+  />
+);
+
+const googleLogin = (disabled, googleResponse, onFailure, action) => (
+  <GoogleLogin
+    google-button="google button"
+    clientId={googgleClientId}
+    disabled={disabled}
+    onSuccess={response => googleResponse(response, 'google')}
+    onFailure={onFailure}
+    render={renderProps => (
+      <AuthModalButton
+        clicked={renderProps.onClick}
+        disabled={renderProps.disabled}
+        iconType="google"
+        color="#4285F4"
+      >
+        <span className="spaceRight">
+          {action}
+          {' '}
+        </span>
+        {' '}
+            with Google
+      </AuthModalButton>
+    )}
+  />
+);
 const SocialAuthModalButtons = (props) => {
   const {
-    twitterResponse,
-    facebookResponse,
-    googleResponse,
+    twitterResponse, facebookResponse, googleResponse,
     action, disabled, onFailure, cancel,
   } = props;
 
-  const googgleClientId = '1005118583442-8vl6g21ovisrkasced1n5ea0vpcms2ge.apps.googleusercontent.com';
-  const facebookAppId = '424643588295726';
   return (
     <div data-test="social-button-test">
       <AuthModalButton
@@ -31,50 +78,8 @@ const SocialAuthModalButtons = (props) => {
         {' '}
         with twitter
       </AuthModalButton>
-      <FacebookLogin
-        facebook-button="facebook button"
-        appId={facebookAppId}
-        autoLoad={false}
-        fields="name,email,picture"
-        callback={response => facebookResponse(response, 'facebook')}
-        render={renderProps => (
-          <AuthModalButton
-            iconType="facebook"
-            color="#3C5A99"
-            clicked={renderProps.onClick}
-            theme="filled"
-          >
-            <span className="spaceRight">
-              {action}
-              {' '}
-            </span>
-            {' '}
-            with Facebook
-          </AuthModalButton>
-        )}
-      />
-      <GoogleLogin
-        google-button="google button"
-        clientId={googgleClientId}
-        disabled={disabled}
-        onSuccess={response => googleResponse(response, 'google')}
-        onFailure={onFailure}
-        render={renderProps => (
-          <AuthModalButton
-            clicked={renderProps.onClick}
-            disabled={renderProps.disabled}
-            iconType="google"
-            color="#4285F4"
-          >
-            <span className="spaceRight">
-              {action}
-              {' '}
-            </span>
-            {' '}
-            with Google
-          </AuthModalButton>
-        )}
-      />
+      {facebookLogin(facebookResponse, action)}
+      {googleLogin(disabled, googleResponse, onFailure, action)}
     </div>
   );
 };
