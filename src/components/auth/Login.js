54911b4e45295bb3c6d74e26330d92d750035714
wangonya <kwangonya@gmail.com>
2019-07-27 12:16:51 +0300

    reduce LoginDetails function

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,39 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-  Form, Icon, Input, Button,
-} from 'antd';
+import { Form } from 'antd';
+import { formItem, formButton } from '../../utils/formElements';
 
 const LoginDetails = (props) => {
   const { submit, form, onCancel } = props;
   return (
     <Form onSubmit={event => submit(event, form)} style={{ textAlign: 'left' }}>
-      <Form.Item data-test="username-input" label="Username">
-        {form.getFieldDecorator('username', {
-          rules: [{ required: true, message: 'Please input your username!' }],
-        })(<Input
-          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          placeholder="john"
-        />)}
-      </Form.Item>
-      <Form.Item data-test="password-input" label="Password">
-        {form.getFieldDecorator('password', {
-          rules: [{ required: true, message: 'Please input your Password!' }],
-        })(<Input
-          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          type="password"
-          placeholder="*****"
-        />)}
-      </Form.Item>
-      <Form.Item>
-        <Button
-          type="primary"
-          htmlType="submit"
-          block
-        >
-          Log in
-        </Button>
-      </Form.Item>
+      {formItem(form, 'username-input', 'Username',
+        'username', 'user', 'text', 'john')}
+      {formItem(form, 'password-input', 'Password',
+        'password', 'lock', 'password', 'johndoe@example.com')}
+      {formButton('Log in')}
       <Link to="/passwordreset" onClick={onCancel}>Forgot Password?</Link>
     </Form>
   );
