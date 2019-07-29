8523b6cc7ceab238b9f2007fa9fa99bd03caa7ed
wangonya <kwangonya@gmail.com>
2019-07-29 14:17:28 +0300

    shorten login reducer

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -9,40 +9,16 @@ export const initialState = {
 export default function (state = initialState, action) {
   switch (action.type) {
     case actionTypes.GOOGLE_AUTH:
-      return {
-        ...state,
-        isLoggedIn: true,
-        actionCalled: true,
-      };
+      return { ...state, isLoggedIn: true, actionCalled: true };
     case actionTypes.FACEBOOK_AUTH:
-      return {
-        ...state,
-        isLoggedIn: true,
-        actionCalled: true,
-      };
+      return { ...state, isLoggedIn: true, actionCalled: true };
     case actionTypes.TWITTER_AUTH:
-      return {
-        ...state,
-        isLoggedIn: true,
-        actionCalled: true,
-      };
-
+      return { ...state, isLoggedIn: true, actionCalled: true };
     case actionTypes.LOG_IN:
-      return {
-        ...state,
-        isLoggedIn: true,
-        actionCalled: true,
-      };
+      return { ...state, isLoggedIn: true, actionCalled: true };
     case actionTypes.LOG_OUT:
-      return {
-        ...state,
-        isLoggedIn: false,
-        actionCalled: true,
-      };
+      return { ...state, isLoggedIn: false, actionCalled: true };
     default:
-      return {
-        ...state,
-        actionCalled: true,
-      };
+      return { ...state, actionCalled: true };
   }
 }
