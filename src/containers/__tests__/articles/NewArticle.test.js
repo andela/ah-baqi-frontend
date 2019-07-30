037c56cc6a95e8606164de1625fc290043a7c77d
wangonya <kwangonya@gmail.com>
2019-07-30 15:55:21 +0300

    add tests

--- src/containers/__tests__/articles/NewArticle.test.js
+++ src/containers/__tests__/articles/NewArticle.test.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 
 import { containerStore, componentRenders } from '../../../utils/testUtils';
 import CreateUpdateArticle, { UnconnectedCreateUpdateArticle } from '../../articles/NewArticle';
 import ArticlesForm from '../../../components/articles/ArticlesForm';
 
 const store = containerStore({});
-const component = shallow(<CreateUpdateArticle store={store} />).dive().dive();
+const component = mount(<CreateUpdateArticle store={store} />);
 
 
-describe('<ArticlePage /> rendering', () => {
+describe('<ArticlePage />', () => {
   test('should render empty when login is false', () => {
     componentRenders(component, 'article form page');
   });
 
-  test('should render sform if login is true', () => {
+  test('should render form if login is true', () => {
     const articleData = {
       id: 1,
       body: 'this is my body',
@@ -24,41 +24,36 @@ describe('<ArticlePage /> rendering', () => {
     };
     localStorage.setItem('isLoggedIn', true);
     localStorage.setItem('user_id', '1');
-    const componentAfterLogin = shallow(<CreateUpdateArticle
+    const componentAfterLogin = mount(<CreateUpdateArticle
       store={store}
       articleData={articleData}
       create={false}
-    />)
-      .dive().dive();
+    />);
     const articleform = componentAfterLogin.find(ArticlesForm);
     expect(articleform).toHaveLength(1);
   });
   test('renders correctly', () => {
-    const createArticleMock = jest.fn();
-    const editArticleMock = jest.fn();
     const articleData = {
       id: 1,
       body: 'this is my body',
       title: 'Page Title',
       description: 'article description',
       slug: 'page-title_username',
+      image: 'imageUrl',
     };
 
     const props = {
       article: articleData,
-      create: createArticleMock,
-      editArticle: editArticleMock,
+      create: true,
+      createArticle: jest.fn(),
+      editArticle: jest.fn(),
+      handleSubmit: jest.fn(),
     };
-    const wrapper = shallow(<UnconnectedCreateUpdateArticle />);
-    const handleSubmitMock = jest.fn();
-    wrapper.instance().handleSubmit = handleSubmitMock;
-    handleSubmitMock();
-    expect(handleSubmitMock).toHaveBeenCalled();
-    const commentWrapper = wrapper.find("[data-test='comments-container-content']").children();
-    expect(commentWrapper).toHaveLength(0);
-    const wrapperItem = mount(shallow(<UnconnectedCreateUpdateArticle {...props} />).get(0));
-    const form = wrapperItem.find("[data-test='the_form']");
+    const wrapper = mount(<UnconnectedCreateUpdateArticle {...props} />);
+    const form = wrapper.find("[data-test='articles-form']");
     expect(form).toHaveLength(1);
     form.simulate('submit');
+    const spy = jest.spyOn(wrapper.instance().props, 'createArticle');
+    expect(spy).toHaveBeenCalled();
   });
 });
