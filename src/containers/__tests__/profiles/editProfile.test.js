e18d8493dd204520b3b0ae9ef3fc229661590449
wekesa931 <wekesa931>
2019-07-31 15:37:53 +0300

    editProfile tests

--- src/containers/__tests__/profiles/editProfile.test.js
+++ src/containers/__tests__/profiles/editProfile.test.js
@@ -1,71 +1,31 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-
-import { createStore, applyMiddleware } from 'redux';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../../../reducers/index';
-
-import EditProfile, { UnconnectedEditProfile } from '../../profile/EditProfile';
-import { mockFn } from '../../../utils/testUtils';
+import EditProfile from '../../profile/EditProfile';
 
 const configureStoreItem = (initialState) => {
   const store = applyMiddleware(thunk)(createStore);
   return store(rootReducer, initialState);
 };
 
-const props = {
-  displayModalActions: mockFn,
-};
-const setup = () => {
+describe('<FollowUnfollow /> ', () => {
   const store = configureStoreItem();
-  const wrapper = shallow(
-    <EditProfile store={store} />,
-  ).dive().dive();
-  return wrapper;
-};
+  test('it renders correctly', () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <EditProfile />
+      </Provider>,
+    );
 
-describe('redux properties', () => {
-  const profile = {
-    email: 'mwangi@gmail.com',
-    username: 'mwangi',
-    first_name: 'wangombe',
-    last_name: 'boniface',
-    country: 'kenya',
-    bio: 'I love fish',
-    image: 'https://res.cloudinary.com/wekesa931/image/upload/v1554520694/samples/bike',
-  };
-  describe('`editProfile` action creator', () => {
-    let wrapper;
-    let button;
-    beforeEach(() => {
-      wrapper = shallow(<UnconnectedEditProfile
-        editUserProfile={mockFn}
-        userProfile={profile}
-        {...props}
-      />);
-      button = wrapper.find("[data-test='submit-button']");
-      button.simulate('click', { preventDefault() {} });
-    });
-    test('button renders correctly', () => {
-      expect(button).toHaveLength(1);
-    });
-    test('`getUserProfile` action creator is a function on the props', () => {
-      wrapper = setup();
-      const { editUserProfile } = wrapper.instance().props;
-      expect(editUserProfile).toBeInstanceOf(Function);
-    });
-  });
-  describe('test connected component', () => {
-    test('updates state correctly', async () => {
-      const editedFields = {
-        first_name: 'boloyang',
-        last_name: 'wamnyonyez',
-      };
-      const wrapper = setup();
-      wrapper.instance().editUserProfile = mockFn;
-      wrapper.instance().editUserProfile();
-      await mockFn(editedFields);
-      expect(mockFn).toHaveBeenCalled();
-    });
+    const button = wrapper.find("[data-test='submit-button']");
+    button.at(0).simulate('click');
+
+    const editProfileForm = wrapper.find("[data-test='edit-form']");
+    editProfileForm.at(0).props().onCancel();
+    editProfileForm.at(0).props().onCreate();
+    expect(button).toHaveLength(2);
   });
 });
